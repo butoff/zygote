010c48b5b73d7ce5143db71cf4cabb347fbab0ad
butoff <butoff@yandex-team.ru>
2020-02-02 12:12:14 +0300

    activity

---
+++ app/src/main/java/zygote/MainActivity.java
@@ -0,0 +1,12 @@
+package zygote;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public final class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: insert your code here
+    }
+}
